e7ca8e14783f92d6299cdb03d5c42f05933f7879
kimminkwan <zoog15@naver.com>
2021-07-12 18:59:26 +0900

    Create 0711 7장 실습 오늘의 명언.py

---
+++ 시간순삭 파이썬/0711 7장 실습 오늘의 명언.py
@@ -0,0 +1,11 @@
+import random
+ 
+word = []
+ 
+word.append('고생 없이 얻을 수 있는 진실로 귀중한 것은 하나도 없다.')
+word.append('사람은 사랑 할 때 누구나 시인이 된다.')
+word.append('꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.')
+ 
+dailyQuote = random.choice(word)
+ 
+print(dailyQuote)
