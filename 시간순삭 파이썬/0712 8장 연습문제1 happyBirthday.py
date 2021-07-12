cd2cb2f783dcb452c87ea92afe642621f9ad5b8c
kimminkwan <zoog15@naver.com>
2021-07-12 22:23:18 +0900

    Create 0712 8장 연습문제1 happyBirthday.py
    해피버스데이

---
+++ 시간순삭 파이썬/0712 8장 연습문제1 happyBirthday.py
@@ -0,0 +1,10 @@
+def happyBirthday(name) :
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear ",name)
+    print("Happy Birthday to you!")
+
+
+name = input("이름을 입력하시오 : ")
+
+happyBirthday(name)
