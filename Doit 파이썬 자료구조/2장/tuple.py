4409bf16f6a3faa64e13d035ade91aa9a41d18d5
KimMinKwan <zoog15@naver.com>
2021-07-25 22:17:16 +0900

    tuple

---
+++ Doit 파이썬 자료구조/2장/tuple.py
@@ -0,0 +1,18 @@
+tuple01 = ()
+tuple02 = 1,
+tuple03 = (1,)
+
+tuple04 = 1,2,3  # (1,2,3)
+tuple05 = 1,2,3,   # (1,2,3)
+tuple06 = (1,2,3)  # (1,2,3)
+tuple07 = (1,2,3,)  # (1,2,3)
+tuple08 = 'A','B','C',  # ('A','B','C')
+
+tuple09 = tuple()  # () 빈 튜플
+tuple10 = tuple('ABC')  # ('A','B','C') 문자열의 각 문자로부터 원소를 생성
+tuple11 = tuple([1,2,3])  # (1,2,3) 리스트로부터 원소를 생성
+tuple12 = tuple({1,2,3})  # (1,2,3) 집합으로부터 원소를 생성
+
+tuple13 = tuple(range(7))
+tuple14 = tuple(range(3,8))
+tuple15 = tuple(range(3,13,2))
