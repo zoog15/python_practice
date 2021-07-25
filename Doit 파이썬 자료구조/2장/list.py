71378736f72567be35b4a40ce102e8fb99364545
KimMinKwan <zoog15@naver.com>
2021-07-25 22:17:33 +0900

    list

---
+++ Doit 파이썬 자료구조/2장/list.py
@@ -0,0 +1,15 @@
+list01 = []
+list02 = [1,2,3]
+list03 = ['A','B','C']
+
+list04 = list()  # []빈 리스트
+list05 = list('ABC')  # ['A','B','C'] 문자열의 각 문자로부터 원소를 생성
+list06 = list([1,2,3])  # [1,2,3] 리스트로부터 원소를 생성
+list07 = list((1,2,3))  # [1,2,3] 튜플로부터 원소를 생성
+list08 = list({1,2,3})  # [1,2,3] 집합으로부터 원소를 생성
+
+list09 = list(range(7))  # [0,1,2,3,4,5,6]
+list10 = list(range(3,8))  # [3,4,5,6,7]
+list11 = list(range(3,13,2))  # [3,5,7,9,11]
+
+list12 = [None] * 5  #[None,None,None,None,None]
