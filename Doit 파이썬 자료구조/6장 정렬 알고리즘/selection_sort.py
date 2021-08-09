7fdfb84413cd1002314cd416f62d69fa1f93e329
KimMinKwan <zoog15@naver.com>
2021-08-09 17:25:28 +0900

    단순 선택 정렬

---
+++ Doit 파이썬 자료구조/6장 정렬 알고리즘/selection_sort.py
@@ -0,0 +1,14 @@
+# 단순 선택 정렬 알고리즘 구현하기
+
+from typing import MutableSequence
+
+
+def selection_sord(a: MutableSequence) -> None:
+    """단순 선택 정렬"""
+    n = len(a)
+    for i in range(n - 1):
+        min = i  # 정렬한 부분에서 가장 작은 원소의 인덱스
+        for j in range(i+1, n):
+            if a[j] < a[min]:
+                min = j
+        a[i], a[min] = a[min], a[i]  # 정렬할 부분에서 맨 앞의 원소와 가장 작은 원소를 교환
