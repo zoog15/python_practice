e20cdf4e3c6a8639faff98c75935263c21c08910
KimMinKwan <zoog15@naver.com>
2021-08-10 22:25:06 +0900

    # 브루트 포스법으로 문자열 검색하기

---
+++ Doit 파이썬 자료구조/7장 문자열 검색/bf_match.py
@@ -0,0 +1,28 @@
+# 브루트 포스법으로 문자열 검색하기
+
+def br_match(txt: str, pat: str) -> int:  # txt(텍스트) : 검사받는 문자열, pat(패턴) : 찾고자하는 문자열
+    """브루트 포스법으로 문자열 검색"""
+    pt = 0  # txt를 따라가는 커서
+    pp = 0  # pat을 따라가는 커서
+
+    while pt != len(txt) and pp != len(pat):
+        if txt[pt] == pat[pp]:
+            pt += 1
+            pp += 1
+        else:
+            pt = pt - pp + 1
+            pp = 0
+
+    return pt - pp if pp == len(pat) else -1
+
+
+if __name__ == '__main__':
+    s1 = input('텍스트를 입력하세요.: ')  # 텍스트용 문자열
+    s2 = input('패턴을 입력하세요.: ')  # 패턴용 문자열
+
+    idx = br_match(s1,s2)
+
+    if idx == -1:
+        print('텍스트 안에 패턴이 존재하지 않습니다.')
+    else:
+        print(f'{(idx + 1)}번째 문자가 일치합니다.')
